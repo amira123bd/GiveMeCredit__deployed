63f8625b099f39348a8e8739162e7e9e4b1797d8
amira123bd <bedouiamira123@gmail.com>
2023-06-14 11:43:52 +0100

    front-end done

--- application.py
+++ application.py
@@ -26,6 +26,21 @@ def predict_credit():
     return jsonify(output[0])
 
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    print(data)
+    new_data=np.array(data).reshape(1, -1)
+    print(new_data)
+    output=model.predict(new_data)[0]
+    if(output==0):
+        return render_template("home.html",prediction_text="This person is not eligible")
+    else:
+        return render_template("home.html",prediction_text="This person is eligible")
+    
+
+     
+
 
 
 if __name__=="__main__":
